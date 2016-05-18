39381c83f04500bce98d3c262e1d01807d98e352
root <yestab123@gmail.com>
2016-05-19 07:02:52 +0800

    del file

--- bb_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef _HEAD_CONFIG_
-#define _HEAD_CONFIG_
-
-
-
-#endif
--- bb_malloc.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HEAD_MALLOC_
-#define _HEAD_MALLOC_
-
-void *bb_malloc(int size);
-void  bb_free(void *point);
-
-#endif
